b4acabaedd83c09437010d79c582485ddca21e9b
Daniel Bell <danielcomriebell@gmail.com>
2017-09-21 17:56:15 -0400

    Added about page, and homepage styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   imageElement:Array<String> = ['./assets/images/001.jpg','./assets/images/002.jpg','./assets/images/003.jpg','./assets/images/004.jpg'];
 
-
   constructor() { }
 
   ngOnInit() {
