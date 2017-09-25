de4b253a6ef753bcc951f8793429f5bd489afaf6
Daniel Bell <danielcomriebell@gmail.com>
2017-09-25 17:01:42 -0400

    changed layout of images on lookbook page

--- src/app/lookbook/lookbook.component.ts
+++ src/app/lookbook/lookbook.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lookbook.component.scss']
 })
 export class LookbookComponent implements OnInit {
-  collectionElement:Array<String> = ['./assets/first-collection/009.jpg','./assets/first-collection/012.jpg','./assets/first-collection/005.jpg','./assets/first-collection/004.jpg','./assets/first-collection/005.jpg','./assets/first-collection/006.jpg','./assets/first-collection/007.jpg','./assets/first-collection/008.jpg','./assets/first-collection/009.jpg','./assets/first-collection/010.jpg','./assets/first-collection/011.jpg','./assets/first-collection/012.jpg','./assets/first-collection/013.jpg','./assets/first-collection/014.jpg','./assets/first-collection/015.jpg','./assets/first-collection/016.jpg',];
+  collectionElement:Array<String> = ['./assets/first-collection/016.jpg','./assets/first-collection/012.jpg','./assets/first-collection/010.jpg','./assets/first-collection/004.jpg','./assets/first-collection/007.jpg','./assets/first-collection/013.jpg','./assets/first-collection/011.jpg','./assets/first-collection/018.jpg' ];
   constructor() { }
 
   ngOnInit() {
