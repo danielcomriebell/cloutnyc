c98b187c70ef74ef8b37955a372a3a738677c086
Daniel Bell <danielcomriebell@gmail.com>
2017-09-29 16:55:03 -0400

    Added flag for update vs add button

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,18 +10,21 @@ import { AngularFireDatabase } from 'angularfire2/database';
 })
 export class DashboardComponent implements OnInit {
   courses: any[];
-
+  updateFlag: number;
   constructor(private db: AngularFireDatabase, private authService: AuthService, private router: Router) {
     // courses: any[];
     db.list(`/users/${this.authService.currentUserId}`)
       .subscribe(courses =>{
         this.courses=courses;
-        console.log(this.courses);
+        // console.log(this.courses);
+        // console.log("the length is" + this.courses.length);
+        this.updateFlag = this.courses.length;
+
       });
 
     // db.list('/courses').push('new choppa');
     // db.list('/courses').push('YUH');
-
+    // this.updateFlag = this.courses.length;
   }
 
   pushData(gamertag: string, gamertag2:string, instagram_handle:string){
@@ -31,6 +34,7 @@ export class DashboardComponent implements OnInit {
     this.db.object(`/users/${this.authService.currentUserId}`).update({name: gamertag});
     this.db.object(`/users/${this.authService.currentUserId}`).update({customer_id: gamertag2});
     this.db.object(`/users/${this.authService.currentUserId}`).update({instagram_handle: instagram_handle});
+    // this.updateFlag = true;
     // this.db.object(`/users/${this.authService.currentUserId}`).update({status: "under review"});
 
 
